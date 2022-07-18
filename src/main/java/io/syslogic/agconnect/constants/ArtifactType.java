2f20cc8552b7ae372a2a117ee063c958b169d0d9
Martin Zeitler <martin@syslogic.io>
2022-07-18 09:45:49 +0200

    constants moved into their own package.

---
+++ src/main/java/io/syslogic/agconnect/constants/ArtifactType.java
@@ -0,0 +1,11 @@
+package io.syslogic.agconnect.constants;
+
+/**
+ * Artifact Types
+ *
+ * @author Martin Zeitler
+ */
+public class ArtifactType {
+    public static String AAB = "aab";
+    public static String APK = "apk";
+}
--- src/main/java/io/syslogic/agconnect/constants/EndpointUrl.java
+++ src/main/java/io/syslogic/agconnect/constants/EndpointUrl.java
@@ -1,11 +1,11 @@
-package io.syslogic.agconnect.model;
+package io.syslogic.agconnect.constants;
 
 /**
- * API Endpoints.
+ * API Endpoint URL
  *
  * @author Martin Zeitler
  */
-public class Endpoint {
+public class EndpointUrl {
     public static String OAUTH2_TOKEN           = "https://connect-api.cloud.huawei.com/api/oauth2/v1/token";
     public static String PUBLISH_UPLOAD_URL     = "https://connect-api.cloud.huawei.com/api/publish/v2/upload-url";
     public static String PUBLISH_APP_ID_LIST    = "https://connect-api.cloud.huawei.com/api/publish/v2/appid-list";
@@ -16,6 +16,7 @@ public class Endpoint {
     public static String PUBLISH_PHASED_STATUS  = "https://connect-api.cloud.huawei.com/api/publish/v2/phased-release/state";
     public static String PUBLISH_PHASED_RELEASE = "https://connect-api.cloud.huawei.com/api/publish/v2/phased-release";
     public static String PUBLISH_ON_SHELF_TIME  = "https://connect-api.cloud.huawei.com/api/publish/v2/on-shelf-time";
+    public static String PUBLISH_COMPILE_STATUS = "https://connect-api.cloud.huawei.com/api/publish/v2/package/compile/status";
 
     /** AppGallery Connect API */
     public static String CONNECT_API_CONSOLE    = "https://developer.huawei.com/consumer/en/service/josp/agc/index.html#/ups/9249519184595983326";
