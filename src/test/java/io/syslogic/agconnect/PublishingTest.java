354ff0d98a0fe76f15ea93c7f2cd056884d703f6
Martin Zeitler <martin@syslogic.io>
2022-07-23 13:00:30 +0200

    functional test set up.

--- src/test/java/io/syslogic/agconnect/PublishingTest.java
+++ src/test/java/io/syslogic/agconnect/PublishingTest.java
@@ -1,12 +1,16 @@
 package io.syslogic.agconnect;
 
 import org.gradle.api.Project;
-import org.gradle.api.plugins.PluginManager;
 import org.gradle.internal.impldep.junit.framework.TestCase;
-import org.gradle.testfixtures.ProjectBuilder;
-
+import org.gradle.internal.impldep.org.apache.commons.io.FileUtils;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Publishing {@link TestCase}
@@ -15,34 +19,38 @@ import org.junit.jupiter.api.Test;
  */
 class PublishingTest extends TestCase {
 
-    String pluginId = "io.syslogic.agconnect.publishing";
+    String identifier = "io.syslogic.agconnect.publishing";
+    @TempDir File testProjectDir;
+    File buildFile;
+
+    Project project;
+
     String clientId;
     String secret;
-    Project project;
 
     @BeforeEach
     public void setup() {
-        this.project = ProjectBuilder.builder().build();
-        PluginManager pm = this.project.getPluginManager();
-        pm.apply(this.pluginId);
+        this.buildFile = new File(testProjectDir, "build.gradle");
+        try {
+            //noinspection deprecation
+            FileUtils.writeStringToFile(this.buildFile, "plugins {" +
+                "id 'com.android.application'" +
+                "id 'com.huawei.agconnect'" +
+                "id '" + this.identifier + "'" +
+            "}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void credentialsTest() {
-        // assertNotNull(this.clientId);
-        // assertNotNull(this.secret);
-        assertTrue(true);
-    }
+    public void publishReleaseAabTest() {
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(this.testProjectDir)
+                .withArguments(":mobile:publishReleaseAab")
+                .withPluginClasspath()
+                .build();
 
-    @Test
-    public void uploadUrlTaskTest() {
-        // assertNotNull(this.project.getTasks().getByName("uploadUrl"));
-        assertTrue(true);
-    }
-
-    @Test
-    public void publishTaskTest() {
-        // assertNotNull(this.project.getTasks().getByName("uploadPackage"));
-        assertTrue(true);
+        assertNotNull(result.getOutput());
     }
 }
