0aa78ff2d4270e79f90cc602d4b2d4390b7355ee
Martin Zeitler <martin@syslogic.io>
2022-07-25 23:34:49 +0200

    unused import.

--- src/test/java/io/syslogic/agconnect/BaseTestCase.java
+++ src/test/java/io/syslogic/agconnect/BaseTestCase.java
@@ -23,7 +23,6 @@ import java.io.PrintWriter;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
