64d11096544f12d9434d428c5dce74032a91730f
Martin Zeitler <martin@syslogic.io>
2023-09-08 21:32:49 +0200

    passing the private repository URL.

--- .space.kts
+++ .space.kts
@@ -4,6 +4,8 @@
  * For more info, see https://www.jetbrains.com/help/space/automation.html
  */
 
+/** Publish Terminal */
 job("Build") {
+    env["JB_SPACE_MAVEN_REPO"] = "{{ project:JB_SPACE_MAVEN_REPO }}"
     gradlew("amazoncorretto:17-alpine", "build")
 }
