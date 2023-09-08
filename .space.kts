cd74144f934b559b366f5c8b37cb0e55c3b72187
Martin Zeitler <martin@syslogic.io>
2023-09-08 21:39:26 +0200

    job renamed.

--- .space.kts
+++ .space.kts
@@ -4,8 +4,8 @@
  * For more info, see https://www.jetbrains.com/help/space/automation.html
  */
 
-/** Build library */
-job("Build library") {
+/** Build AGConnect publishing */
+job("Build AGConnect publishing") {
     env["JB_SPACE_MAVEN_REPO"] = "{{ project:JB_SPACE_MAVEN_REPO }}"
-    gradlew("amazoncorretto:17-alpine", "build")
+    gradlew("amazoncorretto:17-alpine", "build publish")
 }
