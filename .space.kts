b20823dbb8cc2acd3feb96e968254b606f66fa49
Martin Zeitler <martin@syslogic.io>
2024-12-22 22:13:37 +0100

    Gradle 8.12

--- .space.kts
+++ .space.kts
@@ -9,18 +9,12 @@ job("Build AGConnect publishing") {
     startOn {
         gitPush { enabled = false }
     }
-    parameters {
-        text("GRADLE_TASKS", value = "build", description = "Gradle tasks") {
-            options("build", "build publish") {
-                allowMultiple = false
-            }
-        }
-    }
     container(displayName = "Gradle build", image = "{{ project:DOCKER_IMAGE }}:lts") {
         env["JB_SPACE_MAVEN_REPO"] = "{{ project:JB_SPACE_MAVEN_REPO }}"
-        env["GRADLE_TASKS"] = "{{ GRADLE_TASKS }}"
-        shellScript {
-            content = "gradle --init-script $mountDir/system/gradle/init.gradle -Dorg.gradle.parallel=false ${'$'}GRADLE_TASKS"
+        env["GRADLE_USER_HOME"] = "{{ project:GRADLE_USER_HOME }}"
+        kotlinScript { api ->
+            api.gradle("build")
+            api.gradle("publish")
         }
     }
 }
